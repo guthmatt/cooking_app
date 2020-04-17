594ed98d74b3c104b0f907670c27260ecd4ebd69
Matt <matt@mkguthrie.com>
2020-04-17 07:59:10 -0600

    node project init

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express"),
+    app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Confetti Cuisine");
+});
+
+app.listen(app.get("port"), () => {
+    console.log(
+        `Server is up and running at http://localhost:${app.get("port")}`
+    );
+});
