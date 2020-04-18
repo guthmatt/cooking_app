755d601a27f11049a10299286063c7d9481026a8
Matt <matt@mkguthrie.com>
2020-04-18 16:52:14 -0600

    setup db with mongoose

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ const express = require("express"),
     app = express(),
     homeController = require("./controllers/homeController"),
     errorController = require("./controllers/errorController"),
+    subscribersController = require("./controllers/subscribersController"),
     layouts = require("express-ejs-layouts");
+
+const mongoose = require("mongoose");
+mongoose.connect(
+    "mongodb://localhost:27017/confetti_cuisine",
+    {useNewUrlParser: true}
+);
 
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT || 3000);
@@ -21,8 +28,11 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.get("/subscribers", subscribersController.getAllSubscribers);
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
+
 app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showSignUp);
 app.post("/contact", homeController.postedSignUpForm);
 
 app.use(errorController.pagenotFoundError);
